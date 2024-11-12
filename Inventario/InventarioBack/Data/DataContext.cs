17b6e646268a7fdfb18c84ecd92e251362cc5ab9
Sala K 401 <salak401@salasfr.itm.edu.co>
2024-11-12 15:51:19 -0500

    Nuevos cambios realizados

---
+++ Inventario/InventarioBack/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using InventarioControlador.Entidades;
+namespace InventarioBack.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Tipo> Tipos { get; set; }
+
+    // Para crear index y evitar repeticiones de nombres de los tipos de categorias
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    {
+
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tipo>().HasIndex(c => c.Name).IsUnique();
+
+    }
+
+
+}
